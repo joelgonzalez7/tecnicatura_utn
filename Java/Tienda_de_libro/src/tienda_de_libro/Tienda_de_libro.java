78eb670ced0b51f722f585ba02976b85ab148ee9
joelgonzalez7 <joelious7@gmail.com>
2025-05-18 21:46:41 -0300

    este es el archivo de practica

---
+++ Java/Tienda_de_libro/src/tienda_de_libro/Tienda_de_libro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda_de_libro;
+
+/**
+ *
+ * @author Joe
+ */
+public class Tienda_de_libro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
